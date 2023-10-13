7f2630f3d50063919c7970ba8b38116560368fbc
DavySousa <84909226+DavySousa017@users.noreply.github.com>
2023-10-12 21:31:13 -0300

    [UPDATE] NewTask

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        blue: '#4EA8DE',
-        purpleDark: '#5E60CE',
-        personblack: '#0D0D0D',
-        persongray: '#1A1A1A',
-        persongray_500: '#262626'
-      }
+        blue: "#4EA8DE",
+        purpleDark: "#5E60CE",
+        personblack: "#0D0D0D",
+        persongray: "#1A1A1A",
+        persongray_500: "#262626",
+      },
     },
   },
   plugins: [],
-}
+};
